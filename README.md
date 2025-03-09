46ec2f259a77c6556c7a4e46ff73cf1c69c41d1f
Jaanuszek <kubex564@gmail.com>
2025-03-09 18:15:07 +0100

    added map and implement function to draw next satellite posiotion on map

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
+```
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
 
+(Ubuntu) If you have a problem with:
+```
+ UserWarning: FigureCanvasAgg is non-interactive, and thus cannot be shown
+  plt.show()
+```
+Use: `sudo apt install --reinstall qtbase5-dev qtwayland5 libqt5gui5 libqt5widgets5 libxcb-xinerama0 libxcb-cursor0`
--- fetchAPI.py
+++ fetchAPI.py
@@ -1,101 +1,93 @@
-# import json
-# import requests
-# import time 
-# from skyfield.api import load, EarthSatellite
-# from datetime import datetime
-
-# print("SIEMA ENIU")
-
-# response = requests.get("https://api.spacexdata.com/v4/starlink")
-
-# response_json = response.json()
-# # response_dict = json.loads(response.text)
-# # print(response.text)
-# # print(response_dict)
-# print(response.status_code)
-# if(response.status_code == 200):
-#     print("Succesfully fetched")
-#     # print(response_json)
-#     # if isinstance(response_json, list) and len(response_json) > 0:
-#     #     last_update = response_json[0].get('updated_at', 'brak daty aktualizacji')
-#     #     last_update = response_json[0].get('updated_at', 'brak daty aktualizacji')
-#     #     print(f'Ostatnia aktualizacja: {last_update}')
-#     # else:
-#     #     print("Brak danych w odpowiedzi.")
-#     for i in response_json:
-#         print(i["spaceTrack"]["TLE_LINE0"])
-#         print(i["spaceTrack"]["TLE_LINE1"])
-#         print(i["spaceTrack"]["TLE_LINE2"])
-#         tle_name=i["spaceTrack"]["TLE_LINE0"]
-#         tle_line1=i["spaceTrack"]["TLE_LINE1"]
-#         tle_line2=i["spaceTrack"]["TLE_LINE2"]
-#         starlinkInfo=EarthSatellite(tle_line1, tle_line2, tle_name, load.timescale())
-#         t = load.timescale().now()
-#         geocentric = starlinkInfo.at(t)
-#         # lat, lon, alt = geocentric.subpoint()
-#         subpoint = geocentric.subpoint()
-#         lat = subpoint.latitude.degrees
-#         lon = subpoint.longitude.degrees
-#         alt = subpoint.elevation.km
-#         print(f"Szerokość: {lat}, Długość: {lon}, Wysokość: {alt} km")
-#         # print(i)
-#         # print("lognitude: ", i["longitude"])
-#         # print("latitude: ", i["latitude"])
-#         time.sleep(1)
-
-#     # for i in response_dict:
-#         # print("key: ", i, "val: ", response_dict[i])
-# elif(response.status_code == 400):
-#     print("Response error 400")
-# else:
-#     print("Response error 500")
-
+import argparse
+import sys
 import requests
-import folium
 import time
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
 
-API_KEY = ""
+parser = argparse.ArgumentParser(
+    prog = 'Starlink-tracker',
+    description= 'Program for tracking starlinks xd'
+    )
+parser.add_argument(
+    '--api_key', '-k', 
+    dest='apiKey',
+    type=str, 
+    help='Add api key',
+    required=True
+    )
+args = parser.parse_args()
+
+if (len(args.apiKey) != 25):
+    print("An api key of incorrect length was entered")
+    exit(1)
+
+API_KEY = args.apiKey
 
 SAT_ID = 56144
 
-# Twoje współrzędne geograficzne (opcjonalnie dla predykcji przelotów)
-LAT = 52.2298  # szerokość geograficzna (np. Warszawa)
-LON = 21.0122  # długość geograficzna
-ALT = 100      # wysokość w metrach
+LAT = 52.2298
+LON = 21.0122
+ALT = 100
 
-m = folium.Map(location=[0, 0], zoom_start = 2)
 path = []
 
 def getStarlikPosiotion():
-    # Endpoint API do pobrania pozycji satelity w czasie rzeczywistym
-    url = f"https://api.n2yo.com/rest/v1/satellite/positions/{SAT_ID}/{LAT}/{LON}/{ALT}/1001/?apiKey={API_KEY}"
+    url = f"https://api.n2yo.com/rest/v1/satellite/positions/{SAT_ID}/{LAT}/{LON}/{ALT}/1/?apiKey={API_KEY}"
 
-    # Wysyłanie zapytania do API
     response = requests.get(url)
 
-    # Sprawdzanie odpowiedzi
     if response.status_code == 200:
         data = response.json()
-        # print("Pozycja satelity:")
-        # print(f"Szerokość: {data['positions'][0]['satlatitude']}")
-        # print(f"Długość: {data['positions'][0]['satlongitude']}")
-        # print(f"Wysokość: {data['positions'][0]['sataltitude']} km")
-        position = data['positions']
-        # lat, lon = position['satlatitude'], position['satlongitude']
-        path.extend([(pos['satlatitude'], pos['satlongitude']) for pos in position])
-        # path.append((lat,lon))
-        return [(pos['satlatitude'], pos['satlongitude']) for pos in position]
+        SatelitePosition = data['positions']
+        path.extend([(pos['satlatitude'], pos['satlongitude']) for pos in SatelitePosition])
+        return [(pos['satlatitude'], pos['satlongitude']) for pos in SatelitePosition]
     else:
         print("Błąd:", response.status_code, response.text)
         return []
 
-for _ in range(1):
+def updateMap(frame):
+    global posList
+
     pathReceived = getStarlikPosiotion()
-    # if pathReceived:
-    for (lat, lon) in path:
-        folium.Marker(location=[lat, lon], popup="Starlink").add_to(m)
-    time.sleep(5)
+    lat, lon = zip(*pathReceived)
 
-folium.PolyLine(path, color = "red", weight = 2.5, opacity=1).add_to(m)
+    lons, lats = map(lon, lat)
+    # Joining two lists lons and lats into a list of (lons,lats) tuples
+    posList.extend(zip(lons, lats))
 
-m.save("starlink_map.html")
+    ax.clear()
+    map.drawcoastlines(linewidth=0.5)
+    map.drawmapboundary(fill_color='aqua')
+    map.fillcontinents(color='coral', lake_color='aqua')
+    map.drawcountries(linewidth=0.25)
+    
+    for x,y in posList:
+        map.plot(x,y, marker='o', color='r')
+
+    plt.title("siemaEniu")
+
+
+fig, ax = plt.subplots(figsize=(10,6))
+map = Basemap(projection='mill', ax=ax)
+map.drawcoastlines(linewidth=0.5)
+map.drawmapboundary(fill_color='aqua')
+map.fillcontinents(color='coral', lake_color='aqua')
+map.drawcountries(linewidth=0.25)
+
+posList = []
+
+# pathReceived = getStarlikPosiotion()
+# lat, lon = zip(*pathReceived)
+# # for point in pathReceived:
+# #     lat = point[1]
+# #     lon = point[0]
+# lons, lats = map(lon, lat)
+# map.plot(lons, lats, marker = 'o', color='r')
+
+
+# plt.title("siemaEniu")
+ani = FuncAnimation(fig, updateMap, interval=5000, cache_frame_data=False)
+plt.show()
