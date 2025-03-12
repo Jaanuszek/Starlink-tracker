f2ca5f7f3cbe6df416462e822993c6be048eb5a7
Jaanuszek <kubex564@gmail.com>
2025-03-12 22:03:32 +0100

    added opengl to project

--- StarlinkTracker/StarlinkTracker/main.cpp
+++ StarlinkTracker/StarlinkTracker/main.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
-#include "external/curl/curl.h"
+#include <curl.h>
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+
 
 int main() {
+    // opengl test
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+    }
+    else
+    {
+        std::cout << "GLFW initialized" << std::endl;
+    }
+
+	//libcurl test
     CURL* curl;
     CURLcode res;
 
-    // Inicjalizowanie biblioteki libcurl
     curl_global_init(CURL_GLOBAL_DEFAULT);
 
     // Tworzenie uchwytu dla po³¹czenia
     curl = curl_easy_init();
-
     if (curl) {
         // Ustawianie URL do pobrania
         curl_easy_setopt(curl, CURLOPT_URL, "http://www.example.com");
